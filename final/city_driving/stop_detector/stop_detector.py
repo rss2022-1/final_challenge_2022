c93eee93268c899f5768425f7e786931ad4a274a
Ashley <iash21400@gmail.com>
2022-04-20 20:38:26 +0000

    basic structure for stop detector

--- final/city_driving/stop_detector/stop_detector.py
+++ final/city_driving/stop_detector/stop_detector.py
@@ -8,7 +8,7 @@ from detector import StopSignDetector
 class SignDetector:
     def __init__(self):
         self.detector = StopSignDetector()
-        self.publisher = None #TODO
+        self.publisher = None #TODO: publish location of the stop sign? Just (x, y) relative to car?
         self.subscriber = rospy.Subscriber("/zed/zed_node/rgb/image_rect_color", Image, self.callback)
 
     def callback(self, img_msg):
@@ -17,7 +17,12 @@ class SignDetector:
         bgr_img = np_img[:,:,:-1]
         rgb_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2RGB)
 
-        #TODO: 
+        #TODO: process image
+        stop_sign_present, bounding_box = self.detector.predict(rgb_img)
+
+        #TODO: retrieve coordinates in image of center of stop sign
+
+        #TODO: convert coords to real-world and publish
 
 if __name__=="__main__":
     rospy.init_node("stop_sign_detector")
